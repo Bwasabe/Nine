68b747efc2c63d1f64e065e7fdbd25aa43d0970a
박상빈 <jixg20@gmail.com>
2022-01-06 18:28:07 +0900

    제네릭 이벤트 매니저

--- Assets/1.Scripts/1.Common/EventManager.cs
+++ Assets/1.Scripts/1.Common/EventManager.cs
@@ -9,31 +9,39 @@ public class EventManager<Object> : MonoBehaviour
     #region With Parameter(Action)
     private static Dictionary<string, Action> eventDictionary = new Dictionary<string, Action>();
 
-    public static void StartListening(string eventName , Action listener){
+    public static void StartListening(string eventName, Action listener)
+    {
         Action thisEvent;
-        if(eventDictionary.TryGetValue(eventName , out thisEvent)){
+        if (eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
             thisEvent += listener;
             eventDictionary[eventName] = thisEvent;
         }
-        else{
+        else
+        {
             eventDictionary.Add(eventName, listener);
         }
     }
 
-    public static void StopListening(string eventName , Action listener){
+    public static void StopListening(string eventName, Action listener)
+    {
         Action thisEvent;
-        if(eventDictionary.TryGetValue(eventName , out thisEvent)){
+        if (eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
             thisEvent -= listener;
             eventDictionary[eventName] = thisEvent;
         }
-        else{
+        else
+        {
             eventDictionary.Remove(eventName);
         }
     }
 
-    public static void TriggerEvent(string eventName){
+    public static void TriggerEvent(string eventName)
+    {
         Action thisEvent;
-        if(eventDictionary.TryGetValue(eventName , out thisEvent)){
+        if (eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
             thisEvent?.Invoke();
         }
     }
@@ -43,66 +51,128 @@ public class EventManager<Object> : MonoBehaviour
     #region  Without Parameter(Action)
     private static Dictionary<string, Action<Object>> eventParamDictionary = new Dictionary<string, Action<Object>>();
 
-    public static void StartListening(string eventName , Action<Object> listener){
+    public static void StartListening(string eventName, Action<Object> listener)
+    {
         Action<Object> thisEvent;
-        if(eventParamDictionary.TryGetValue(eventName , out thisEvent)){
+        if (eventParamDictionary.TryGetValue(eventName, out thisEvent))
+        {
             thisEvent += listener;
             eventParamDictionary[eventName] = thisEvent;
         }
-        else{
+        else
+        {
             eventParamDictionary.Add(eventName, listener);
         }
     }
 
-    public static void StopListening(string eventName , Action<Object> listener){
+    public static void StopListening(string eventName, Action<Object> listener)
+    {
         Action<Object> thisEvent;
-        if(eventParamDictionary.TryGetValue(eventName , out thisEvent)){
+        if (eventParamDictionary.TryGetValue(eventName, out thisEvent))
+        {
             thisEvent -= listener;
             eventParamDictionary[eventName] = thisEvent;
         }
-        else{
+        else
+        {
             eventParamDictionary.Remove(eventName);
         }
     }
 
-    public static void TriggerEvent(string eventName , Object param){
+    public static void TriggerEvent(string eventName, Object param)
+    {
         Action<Object> thisEvent;
-        if(eventParamDictionary.TryGetValue(eventName , out thisEvent)){
+        if (eventParamDictionary.TryGetValue(eventName, out thisEvent))
+        {
             thisEvent?.Invoke(param);
         }
     }
 
     #endregion
 
+    
+    #region With Parameter(Func)
+    private static Dictionary<string, Func<Object, Object>> eventParamFuncDictionary = new Dictionary<string, Func<Object, Object>>();
 
+    public static void StartListening(string eventName, Func<Object, Object> listener)
+    {
+        Func<Object, Object> thisEvent;
+        if (eventParamFuncDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent += listener;
+            eventParamFuncDictionary[eventName] = thisEvent;
+        }
+        else
+        {
+            eventParamFuncDictionary.Add(eventName, listener);
+        }
+    }
+
+    public static void StopListening(string eventName, Func<Object, Object> listener)
+    {
+        Func<Object, Object> thisEvent;
+        if (eventParamFuncDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent -= listener;
+            eventParamFuncDictionary[eventName] = thisEvent;
+        }
+        else
+        {
+            eventParamFuncDictionary.Remove(eventName);
+        }
+    }
+
+    public static void FuncParamTriggerEvent(string eventName, Object param)
+    {
+        Func<Object, Object> thisEvent;
+        if (eventParamFuncDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.Invoke(param);
+        }
+    }
+    #endregion
+
+
+    #region Without Parameter(Func)
     private static Dictionary<string, Func<Object>> eventFuncDictionary = new Dictionary<string, Func<Object>>();
 
-    public static void StartListening(string eventName , Func<Object> listener){
+    public static void StartListening(string eventName, Func<Object> listener)
+    {
         Func<Object> thisEvent;
-        if(eventFuncDictionary.TryGetValue(eventName , out thisEvent)){
+        if (eventFuncDictionary.TryGetValue(eventName, out thisEvent))
+        {
             thisEvent += listener;
             eventFuncDictionary[eventName] = thisEvent;
         }
-        else{
+        else
+        {
             eventFuncDictionary.Add(eventName, listener);
         }
     }
 
-    public static void StopListening(string eventName , Func<Object> listener){
+    public static void StopListening(string eventName, Func<Object> listener)
+    {
         Func<Object> thisEvent;
-        if(eventFuncDictionary.TryGetValue(eventName , out thisEvent)){
+        if (eventFuncDictionary.TryGetValue(eventName, out thisEvent))
+        {
             thisEvent -= listener;
             eventFuncDictionary[eventName] = thisEvent;
         }
-        else{
+        else
+        {
             eventFuncDictionary.Remove(eventName);
         }
     }
 
-    public static void FuncTriggerEvent(string eventName , Object param){
+    public static void FuncTriggerEvent(string eventName, Object param)
+    {
         Func<Object> thisEvent;
-        if(eventFuncDictionary.TryGetValue(eventName , out thisEvent)){
+        if (eventFuncDictionary.TryGetValue(eventName, out thisEvent))
+        {
             thisEvent.Invoke();
         }
     }
+    #endregion
+
+
 }
