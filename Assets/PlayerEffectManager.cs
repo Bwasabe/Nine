a0e1ad96312d2c27563520495f0044f3099f3984
pqowp90 <77601017+pqowp90@users.noreply.github.com>
2022-05-29 19:49:35 +0900

    1
    1

--- Assets/PlayerEffectManager.cs
+++ Assets/PlayerEffectManager.cs
@@ -39,9 +39,11 @@ public class PlayerEffectManager : MonoBehaviour
         yield return Yields.WaitForSeconds(0.03f);
         while(loop){
             IPoolObj iPoolObj = PoolManager.GetItem<IPoolObj>("Ghost");
-            iPoolObj.gameObject.SetActive(true);
-            iPoolObj.SetSprite(playerMove.spriteRenderer.sprite ,playerMove.isBack);
-            iPoolObj.transform.position = transform.position;
+            if(iPoolObj){
+                iPoolObj.gameObject.SetActive(true);
+                iPoolObj.SetSprite(playerMove.spriteRenderer.sprite ,playerMove.isBack);
+                iPoolObj.transform.position = transform.position;
+            }
             
             
             time3 += time2;
